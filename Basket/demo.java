6c3b6529046fc7c8e04b5d8eb4f23db3886d6ae8
EmanShabaneh <103412143+EmanShabaneh@users.noreply.github.com>
2024-01-08 11:20:09 +0300

    add object

--- Basket/demo.java
+++ Basket/demo.java
@@ -1,5 +1,9 @@
 package Basket;
 
 public class demo {
-
+	public static void main(String[] args) {
+        Basket_e<Double, Double> b = new Basket_e<>();
+        b.add(2.4);
+        b.add(5.6);
+    }
 }
