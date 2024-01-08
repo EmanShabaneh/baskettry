e362e1fff365e82da713c61e2619e3faf7f0926c
EmanShabaneh <103412143+EmanShabaneh@users.noreply.github.com>
2024-01-08 11:27:51 +0300

    only one by file added

--- Basket/demo.java
+++ Basket/demo.java
@@ -5,5 +5,7 @@ public class demo {
         Basket_e<Double, Double> b = new Basket_e<>();
         b.add(2.4);
         b.add(5.6);
+        System.out.println(b.toString());
+        b.add(8.0);
     }
 }
