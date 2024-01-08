1a947d14cc5be61ec2cecf2c7d9b1829e5f15ce2
EmanShabaneh <103412143+EmanShabaneh@users.noreply.github.com>
2024-01-08 11:42:49 +0300

    Revert "only one by file added"
    This reverts commit e362e1fff365e82da713c61e2619e3faf7f0926c.

--- Basket/demo.java
+++ Basket/demo.java
@@ -5,8 +5,11 @@ public class demo {
         Basket_e<Double, Double> b = new Basket_e<>();
         b.add(2.4);
         b.add(5.6);
+<<<<<<< HEAD
         System.out.println(b.toString());
         b.add(8.0);
         System.out.println(b.toString()); 
+=======
+>>>>>>> parent of e362e1f (only one by file added)
     }
 }
