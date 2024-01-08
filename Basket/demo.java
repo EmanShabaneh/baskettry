be6d65e11cac71ba1f73008d8fd7d03d2e224d9b
EmanShabaneh <103412143+EmanShabaneh@users.noreply.github.com>
2024-01-08 20:31:02 +0300

    Eman's First edit

--- Basket/demo.java
+++ Basket/demo.java
@@ -5,6 +5,7 @@ public class demo {
         Basket_e<Double, Double> b = new Basket_e<>();
         b.add(2.4);
         b.add(5.6);
+        b.add(7.2);
 <<<<<<< HEAD
         System.out.println(b.toString());
         b.add(8.0);
